ee4d7dc5d433b30bfb5643c048d447b2d2abd696
paulabssantos <paulablsantos7@gmail.com>
2022-10-17 18:11:26 -0300

    using djangorestframwork

--- core/queries.py
+++ core/queries.py
@@ -1,4 +1,8 @@
+from unicodedata import name
 from core import models
 
 def all_states():
     return models.State.objects.all()
+
+def update_state():
+    return models.State.objects.filter(name__iexact='amazonas').update(name='Pará',abbreviation='PA')
